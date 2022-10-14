a8afe5c32e2f4421ce4615f09a325808ced340e7
d.kharin <dmuralstroi@mail.ru>
2022-10-14 13:15:23 +0500

    create

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let slides = document.querySelectorAll('.slide');
+
+    slides.forEach((slide) => {
+        slide.addEventListener('click', () => {
+            removeActiveClass();
+            slide.classList.add('active');
+        });
+    });
+
+    function removeActiveClass() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });  
+    }
+})
